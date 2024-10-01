65699a5d769e8d07ddd73d991fd0b8daa3d2765d
Mmaspekere <ngobeniannaraesibe@gmail.com>
2024-10-01 10:26:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+
+function changeimage(){
+    document.getElementById('myimage').src="https://images.unsplash.com/photo-1726074346607-a4882e522d24?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2Mnx8fGVufDB8fHx8fA%3D%3D"
+}
